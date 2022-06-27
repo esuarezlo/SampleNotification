b124a5100d0300c9044163a317da2c1adba16982
Ernesto Suarez <ernesto@amicitech.com>
2022-06-27 12:05:56 -0500

    Initial commit

---
+++ app/src/main/java/org/danp/samplenotification/MainActivity.java
@@ -0,0 +1,26 @@
+package org.danp.samplenotification;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
